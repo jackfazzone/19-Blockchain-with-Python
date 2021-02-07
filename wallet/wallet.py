44a02fbc9b9c71ddd5c450f8a9c0d0a3c6008dfa
Jack Fazzone <anthony.j.fazzone@gmail.com>
2021-02-06 20:49:02 -0600

    wallet complete

--- wallet/wallet.py
+++ wallet/wallet.py
@@ -5,23 +5,27 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 from web3 import Web3
-import bit
-
-print("RAN")
+from eth_account import Account
+from bit import wif_to_key
+from bit import PrivateKeyTestnet
+#Citation: https://ofek.dev/bit/guide/advanced.html
+from bit.network import NetworkAPI
 
 mnemonic = os.getenv('MNEMONIC')
-
+conn = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
+print(mnemonic)
 def derive_wallets(coin):
-    command = f'php -g --mnemonic={mnemonic} --cols=path,address,privkey,pubkey --coin={coin} --numderive=3 --format=json'
+    command = f"php derive -g --mnemonic --cols=path,address,privkey,pubkey --coin={coin} --numderive=3 --format=json"
     p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     output, err = p.communicate()
     p_status = p.wait()
-    keys = json.loads(output)
-    return keys
+    result = json.loads(output)
+    return result
 
 coins = {'eth':derive_wallets(ETH), 'btc-test':derive_wallets(BTCTEST)}
 
-def priv_key_to_account(coin,privkey)
+def priv_key_to_account(coin,index):
+    priv_key = coins[f'{coin}'][index]['privkey']
     if coin == ETH:
         result = Account.privateKeyToAccount(priv_key)
     elif coin == BTCTEST:
@@ -29,8 +33,12 @@ def priv_key_to_account(coin,privkey)
     else:
         result = "Coin type not ETH or BTC-TEST"
     return result
+    
 
-def create_tx(coin,account,to,amount)
+print(priv_key_to_account(ETH,0))
+print(priv_key_to_account(BTCTEST,0))
+
+def create_tx(coin,account,to,amount):
     if coin == ETH:
         gasEstimate = conn.eth.estimateGas({
         "from": account.address,
@@ -43,25 +51,24 @@ def create_tx(coin,account,to,amount)
         "value": amount,
         "gasPrice": conn.eth.gasPrice,
         "gas": gasEstimate,
-        "nonce": conn.eth.getTransactionCount(account.address)
+        "nonce": conn.eth.getTransactionCount(account.address),
+        "chainID": conn.net.chainId
     }
     elif coin == BTCTEST:
-        result = PrivateKeyTestnet.prepare_transaction(account.address, [(to, amount, BTC)])
+        result = PrivateKeyTestnet.prepare_transaction(account.address, [(to, amount, BTCTEST)])
     else:
         result = "Coin type not ETH or BTC-TEST"
     return result
 
-def send_tx(coin,account,to,amount)
+def send_tx(coin,account,to,amount):
     tx = create_tx(coin, account, to, amount)
-    signed_tx = account.sign_transaction(tx)
+    signed = account.sign_transaction(tx)
     if coin== ETH:
-        send = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
+        send = conn.eth.sendRawTransaction(signed.rawTransaction)
         result = send.hex()
-
     elif coin == BTCTEST:
-        result = PrivateKeyTestnet.prepare_transaction(account.address, [(to, amount, BTC)])
+        result = NetworkAPI.broadcast_tx_testnet(signed)
     else:
         result = "Coin type not ETH or BTC-TEST"
     return result
 
-#print(keys)
